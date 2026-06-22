a835ffa714b17e45539dc9de72013937dd8e9e77
Rawvoid <rawvoid@users.noreply.github.com>
2026-06-22 15:55:57 +0800

    refactor: remove debian packaging feature registration

--- deployment/src/main/java/io/github/rawvoid/quarkus/debian/packaging/deployment/QuarkusDebianPackagingProcessor.java
+++ deployment/src/main/java/io/github/rawvoid/quarkus/debian/packaging/deployment/QuarkusDebianPackagingProcessor.java
@@ -1,14 +1,4 @@
 package io.github.rawvoid.quarkus.debian.packaging.deployment;
 
-import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.builditem.FeatureBuildItem;
-
 class QuarkusDebianPackagingProcessor {
-
-    private static final String FEATURE = "quarkus-debian-packaging";
-
-    @BuildStep
-    FeatureBuildItem feature() {
-        return new FeatureBuildItem(FEATURE);
-    }
 }
